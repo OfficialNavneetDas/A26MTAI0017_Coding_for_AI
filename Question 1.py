e13f48a50606b25da4b7f4bc568b248a244f34fd
Navneet Das <officialnavneetdas@gmail.com>
2026-08-03 16:56:34 +0530

    inital commit

--- Question 1.py
+++
@@ -1,8 +0,0 @@
-gpa = input("please enter your gpa (in float):")
-attendance = int(input("enter your attendance:"))
-
-# conditions 
-if gpa>=8.0 and attendance>=75:
-    print("eligible for schlorship")
-else:
-    print("Not eligible for schlorship")
